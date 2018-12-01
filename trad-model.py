4b1543a0ab24f0821d57b59ad17d067772587680
Travis Smith <travis42@gmail.com>
2018-12-01 12:57:03 -0500

    adding probabilistic model WIP

---
+++ trad-model.py
@@ -0,0 +1,114 @@
+import random
+
+height = 100
+width = 100
+
+
+def randomize(grid, width, height):
+    for i in range(0, height):
+        for j in range(0, width):
+            grid[i][j] = random.randint(0, 1)
+
+
+grid_model = [0] * height
+next_grid_model = [0] * height
+for i in range(height):
+    grid_model[i] = [0] * width
+    next_grid_model[i] = [1] * width
+
+
+def next_gen():
+    global grid_model, next_grid_model
+
+    for i in range(0, height):
+        for j in range(0, width):
+            cell = 0
+            count = count_neighbors(grid_model, i, j)
+
+            if grid_model[i][j] == 0:
+                if count == 3:
+                    cell = 1
+            elif grid_model[i][j] == 1:
+                if count == 2 or count == 3:
+                    cell = 1
+            next_grid_model[i][j] = cell
+
+    temp = grid_model
+    grid_model = next_grid_model
+    next_grid_model = temp
+
+
+def count_neighbors(grid, row, col):
+    count = 0
+    if row - 1 >= 0:
+        count = count + grid[row - 1][col]
+    if (row - 1 >= 0) and (col - 1 >= 0):
+        count = count + grid[row - 1][col - 1]
+    if (row - 1 >= 0) and (col + 1 < width):
+        count = count + grid[row - 1][col + 1]
+    if col - 1 >= 0:
+        count = count + grid[row][col - 1]
+    if col + 1 < width:
+        count = count + grid[row][col + 1]
+    if row + 1 < height:
+        count = count + grid[row + 1][col]
+    if (row + 1 < height) and (col - 1 >= 0):
+        count = count + grid[row + 1][col - 1]
+    if (row + 1 < height) and (col + 1 < width):
+        count = count + grid[row + 1][col + 1]
+    return count
+
+
+glider_pattern = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 0],
+                  [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]
+
+glider_gun_pattern = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0,
+     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+# TODO: use this to regen the grid.  By replicating and offsetting, I can
+# keep the GOL going forever. Just replicate whatever exists, offset it,
+# and pile it back on itself.
+def load_pattern(pattern, x_offset=0, y_offset=0):
+    global grid_model
+
+    # init to clear the grid:
+    for i in range(0, height):
+        for j in range(0, width):
+            grid_model[i][j] = 0
+
+    # this is offsetting by y amount, to apply the pattern wherever you like.
+    j = y_offset
+
+    for row in pattern:
+        # offset by x amount.
+        i = x_offset
+        for value in row:
+            grid_model[i][j] = value
+            i = i + 1
+        j = j + 1
+
+
+if __name__ == '__main__':
+    next_gen()
